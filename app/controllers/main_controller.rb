6a9ac910ea1d4ac3cdb4cae0dfb6c571da00a2e7
Kyle Sandage <ksandage08@gmail.com>
2016-02-09 14:31:36 -0600

    First Commit, Got the homepage to start.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -9,3 +9,26 @@
 # Your controller actions go below this line.
 # -----------------------------------------------------------------------------
 
+MyApp.get "/" do
+  # If a GET request is made to the root path, the following line of code
+  # looks for a .erb view file located in the 'views' directory at the given
+  # location. So it's going to look for views/main/welcome.erb.
+  # 
+  # Then it will combine that view file with the layout file and sent the
+  # combined document back to the client.
+  erb :"homepage"
+end
+
+#  MyApp.get "/:str" do
+#    # @str = params[:str]
+#    # @segment = StringSegmenter.new(@str)
+#    erb :"main/str"
+# end
+
+MyApp.post "/str" do
+  @str = params[:string]
+  @segment = StringSegmenter.new(@str)
+  
+  erb :"main/str"
+
+end
