58e54c048f2c241dbb096b46890ff2c0a4ea846f
Beth Haubert <bethany.haubert@gmail.com>
2016-02-09 13:42:46 -0600

    added controller & schema

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -9,3 +9,20 @@
 # Your controller actions go below this line.
 # -----------------------------------------------------------------------------
 
+MyApp.get "/" do
+  # If a GET request is made to the root path, the following line of code
+  # looks for a .erb view file located in the 'views' directory at the given
+  # location. So it's going to look for views/main/welcome.erb.
+  # 
+  # Then it will combine that view file with the layout file and sent the
+  # combined document back to the client.
+  erb :"main/welcome"
+end
+
+MyApp.get "/segment" do
+  x = Segment.new(params["string_to_segment"])
+  x.run_program
+  @words = x.final_words
+
+  erb :"result"
+end
