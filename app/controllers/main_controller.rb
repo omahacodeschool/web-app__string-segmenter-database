a0a89c0fa086ca2cf294798fe02eea332238396f
Bradley Biehl <bradley.biehl@gmail.com>
2016-02-09 17:26:29 -0600

    NameError at /segment undefined local variable or method

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -11,14 +11,23 @@ require './lib/string_segmenter'
 # Your controller actions go below this line.
 # -----------------------------------------------------------------------------
 
+# Show form
 MyApp.get "/" do
   erb :"main/welcome"
 end
 
+# Process form
 MyApp.get "/segment" do
+  # Use the StringSegmenter to get the final words array
   x = StringSegmenter.new(params["string_to_segment"])
   x.run_program
   @words = x.final_words
+
+  # Use the ActiveRecord 'Search' class to access the database
+  @s = Search.new
+  @s.search_string = params[:unsegmented_text]
+  @s.segmented_parts = segmented_words_as_string
+  @s.save
 
   erb :"main/result"
 end
