9a7291899fa04da100af24384a959eab3137b68c
Amy Wall <amymariewall@gmail.com>
2016-02-09 14:15:07 -0600

    Form working and posting

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -9,3 +9,31 @@
 # Your controller actions go below this line.
 # -----------------------------------------------------------------------------
 
+require_relative "../../lib/string_segmenter"
+
+# The path being defined for this controller action is just "/", meaning 
+# the root path. It's the homepage.
+MyApp.get "/" do
+  # If a GET request is made to the root path, the following line of code
+  # looks for a .erb view file located in the 'views' directory at the given
+  # location. So it's going to look for views/main/welcome.erb.
+  # 
+  # Then it will combine that view file with the layout file and sent the
+  # combined document back to the client.
+  erb :"main/welcome"
+end
+
+MyApp.get "/:str" do
+  words = StringSegmenter.new.(params[:str])
+  words.run_program
+  @words = words.final_words
+  erb :"main/show_words"
+end
+
+
+ MyApp.post "/main/show_words" do
+  words = StringSegmenter.new(params[:string])
+  words.run_program
+  @words = words.final_words
+  erb :"/main/show_words"
+end
