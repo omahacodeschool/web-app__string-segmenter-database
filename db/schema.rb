c78f120b631c27f1e15dab6c66a5a247997cd25a
Thorin Butler <thorin.butler@gmail.com>
2016-02-09 14:11:17 -0600

    Odd thing I noticed...

--- db/schema.rb
+++ db/schema.rb
@@ -15,4 +15,18 @@ ActiveRecord::Schema.define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "chains", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "raw"
+    t.string   "processed"
+  end
+
+  create_table "strings", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "raw"
+    t.string   "processed"
+  end
+
 end
